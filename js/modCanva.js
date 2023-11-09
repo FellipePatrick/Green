7aa4d04ea5f7bd5ce26fd3295c71313f0e503ae3
fellipepatrick <fellipe.patrick@escolar.ifrn.edu.br>
2023-11-09 08:35:52 -0300

    reword in modCanvas

--- js/modCanva.js
+++ js/modCanva.js
@@ -2,27 +2,16 @@ import {Robo} from './modRobo.js';
 export const run = (ctx) => {
     const robo = new Robo(ctx);
     document.addEventListener("keydown", function(event) {
-        if (event.key === "ArrowRight" ) {
-            robo.directionRight();
-        }else if(event.key === "ArrowLeft"){
-            robo.directionLeft();
-        }else if(event.key === "ArrowDown"){
-            robo.directionDown();
-        }else if(event.key === "ArrowUp"){
-            robo.directionUp();
-        }else if(event.key === "Enter"){
-            robo.showerPlants();
-        }
+        if (event.key === "ArrowRight" ) robo.directionRight();
+        else if(event.key === "ArrowLeft") robo.directionLeft();
+        else if(event.key === "ArrowDown") robo.directionDown();
+        else if(event.key === "ArrowUp") robo.directionUp();
+        else if(event.key === "Enter") robo.showerPlants();
     });
     document.addEventListener("keyup", function(event) {
-        if (event.key === "ArrowRight") {
-            robo.stopRight();
-        }else if(event.key === "ArrowLeft"){
-            robo.stopLeft();
-        }else if(event.key === "ArrowDown"){
-            robo.stopDown();
-        }else if(event.key === "ArrowUp"){
-            robo.stopUp();
-        }
+        if (event.key === "ArrowRight") robo.stopRight();
+        else if(event.key === "ArrowLeft") robo.stopLeft();
+        else if(event.key === "ArrowDown") robo.stopDown();
+        else if(event.key === "ArrowUp") robo.stopUp();
     });
 }
